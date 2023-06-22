cd43cb9b0b9bcd10d1b5ad3053e5789736d139d2
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2023-06-22 15:17:43 +0200

    correcciones

--- Repaso/Repaso/Program.cs
+++ Repaso/Repaso/Program.cs
@@ -6,9 +6,10 @@ namespace Repaso
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine(SumarNumeros(2, 6));
-            Console.WriteLine("El resultado de la suma es: " + SumarNum());
+            Console.WriteLine(SumarEnteros(2, 5));
+            SumarNumeros(3, 4);
+            Console.WriteLine(SumarNum());
+            SumarNums();
         }
 
         //1) funcion que recibe 2 numeros enteros como parametro y devuelve el resultado de la suma entre los 2 numeros
@@ -21,10 +22,10 @@ namespace Repaso
 
         //2) funcion que recibe 2 numeros enteros como parametros y pinta por consola el resultadao de la suma entre los 2 numeros
 
-        public static int SumarNumeros(int numero1, int numero2)
+        public static void SumarNumeros(int numero1, int numero2)
         {
             int resultado = numero1 + numero2;
-            return resultado;
+            Console.WriteLine("La sume de " + numero1 + " + " + numero2 + " = " + resultado);
         }
 
         //3) funcion que pide al usuario que introduzca un numero entero y luego otro y devuelve el resultado de la suma entre los 2 numeros
@@ -44,6 +45,19 @@ namespace Repaso
         }
 
         //4) funcion que pide al usuario que introduzca un numero entero y luego otro y pinta por consola el resultado de la suma entre los 2 numeros
-        //ESTA HECHO CON EL 3 ARRIBA
+        public static void SumarNums()
+        {
+            int numero1;
+            int numero2;
+            Console.Write("Introduce el primer número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = numero1 + numero2;
+
+            Console.WriteLine("La sume de " + numero1 + " + " + numero2 + " = " + resultado);
+        }
     }
 }
